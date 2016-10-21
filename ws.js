30d5bf82dba7c48148c8d46d452cc037b59f3fa7
joshuarossi <maximojoshuarossi@gmail.com>
2016-10-21 00:46:19 +0000

    pointed websocket at staging

--- ws.js
+++ ws.js
@@ -46,7 +46,7 @@ util.inherits(BitfinexWS, EventEmitter);
  * @constant
  * @type {String}
  */
-BitfinexWS.WebSocketURI = 'wss://api2.bitfinex.com:3000/ws';
+BitfinexWS.WebSocketURI = 'wss://dev-prdn.bitfinex.com:3000/ws'
 
 BitfinexWS.prototype.onMessage = function (msg, flags) {
     msg = JSON.parse(msg);
